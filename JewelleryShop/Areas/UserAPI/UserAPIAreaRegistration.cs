142293cc02ca2fee4969a17f5478e670326d17e7
Đặng Văn Thành Điệp <dangvanthanhdiep2711@gmail.com>
2025-06-28 23:24:54 +0700

    hello

---
+++ JewelleryShop/Areas/UserAPI/UserAPIAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace JewelleryShop.Areas.UserAPI
+{
+    public class UserAPIAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "UserAPI";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "UserAPI_default",
+                "UserAPI/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
